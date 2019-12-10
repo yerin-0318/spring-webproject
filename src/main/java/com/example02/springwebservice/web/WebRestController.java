188527f5cea9fc77d5521ade506a2cb0d4457146
yerin <hanblueblue@gmail.com>
2019-12-10 21:06:19 +0900

    프로젝트 생성

---
+++ src/main/java/com/example02/springwebservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.example02.springwebservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/example02/springwebservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example02.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
